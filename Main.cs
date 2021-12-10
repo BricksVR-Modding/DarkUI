49f569124e9db9efa02d13ab2ce290040ae9c514
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2021-12-09 21:50:59 -0500

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,36 @@
+using MelonLoader;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+namespace DarkMode
+{
+    public static class BuildInfo
+    {
+        public const string Name = "DarkUI"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "A DarkUI mod for BricksVR."; // Description for the Mod.  (Set as null if none)
+        public const string Author = "BelugaTheAxolotl#2134"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class UIset : MelonMod
+    {
+        public override void OnSceneWasInitialized(int buildindex, string sceneName)
+        {
+            GameObject.Find("MenuBoard/Background").GetComponent<Image>().color = new Color(0, 0, 0, 0.75f);
+            var allOBJ = Resources.FindObjectsOfTypeAll<GameObject>();
+            for (var e = 0; e < allOBJ.Length; e++)
+            {
+                if (!allOBJ[e].GetComponent<Button>()) continue;
+                if (allOBJ[e].GetComponent<Image>() && allOBJ[e].transform.GetChild(0).GetComponent<TextMeshProUGUI>())
+                {
+                    allOBJ[e].GetComponent<Image>().color = new Color(0, 0, 0, 0.75f);
+                    allOBJ[e].transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(0.5755f, 0.5755f, 0.5755f, 1);
+                    allOBJ[e].transform.GetChild(0).GetComponent<TextMeshProUGUI>().faceColor = new Color32(255, 255, 255, 255);
+                }
+
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(DarkMode.BuildInfo.Description)]
+[assembly: AssemblyDescription(DarkMode.BuildInfo.Description)]
+[assembly: AssemblyCompany(DarkMode.BuildInfo.Company)]
+[assembly: AssemblyProduct(DarkMode.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + DarkMode.BuildInfo.Author)]
+[assembly: AssemblyTrademark(DarkMode.BuildInfo.Company)]
+[assembly: AssemblyVersion(DarkMode.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(DarkMode.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(DarkMode.UIset), DarkMode.BuildInfo.Name, DarkMode.BuildInfo.Version, DarkMode.BuildInfo.Author, DarkMode.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("BricksVR", "BricksVR")]
